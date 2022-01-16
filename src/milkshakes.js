527c1eec30c22adf22a4770fb91a129806aff103
Tish Faroul <lfaroul14@gmail.com>
2022-01-16 17:13:57 -0500

    first commit

---
+++ src/milkshakes.js
@@ -0,0 +1,31 @@
+const milkshakes = [{
+    id: 1, 
+    title: "Fruit Me",
+    category: "Fruit",
+    price: 8.99,
+    img: "https://images.unsplash.com/photo-1615478503562-ec2d8aa0e24e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80"
+},
+{
+    id:2,
+    title: "Very Berry",
+    category: "Fruit",
+    price: 7.99,
+    img: "https://images.unsplash.com/photo-1600718374662-0483d2b9da44?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1285&q=80"
+},
+{
+    id: 3,
+    title: "Dough-Not stop me now",
+    category: "Dessert",
+    price: 10.00,
+    img: "https://images.unsplash.com/photo-1562164143-8a9f06b11f47?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80"
+},
+{
+    id: 4,
+    title: "Cookies and Cream",
+    category: "Dessert",
+    price: 7.00,
+    img: "https://images.unsplash.com/photo-1624781740834-fbfbf5fd221a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80"
+}
+]
+
+export default milkshakes; 
