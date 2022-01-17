e48556b47cd92e44bdbf3c2794f016f5a0b57732
Tish Faroul <lfaroul14@gmail.com>
2022-01-16 19:12:49 -0500

    changed around the styling

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,10 +6,12 @@ export default function Menu({milkshakes}) {
             {milkshakes.map((menuItem) => {
                 const {id, title, price, img} = menuItem;
                 return <article key={id} className='menu-item'>
-                    <h3 className="title">{title}</h3>
                     <img src={img} alt={title} className='photo'/>
                     <div className='item-info'>
+                    <header>
+                    <h3 className="title">{title}</h3>
                         <h4 className='price'>${price}</h4>
+                        </header>
                     </div>
                 </article>
             })}
